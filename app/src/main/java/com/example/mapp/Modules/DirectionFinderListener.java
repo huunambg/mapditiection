1bf5ec477f2059f830a0802633a4bda900d483aa
huunambg <nonghuunam2k2@gmail.com>
2022-07-08 15:48:03 +0700

    duanhihi

---
+++ app/src/main/java/com/example/mapp/Modules/DirectionFinderListener.java
@@ -0,0 +1,9 @@
+package com.example.mapp.Modules;
+
+import java.util.List;
+
+public interface
+   DirectionFinderListener {
+    void onDirectionFinderStart();
+    void onDirectionFinderSuccess(List<Route> route);
+}
